f752fbc938312b263ef2b24ce7c2e7b971115d0d
PaddyCube <paddy-daun@web.de>
2023-09-17 20:23:38 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ int main(int argc , char **argv)
 	}
 	ros::Publisher lidar_pub = nh.advertise<sensor_msgs::LaserScan>("LiDAR/LD06", 1); /*create a ROS topic */
 	
-    ros::Rate rate(100.0);
+    ros::Rate rate(20.0);
 	while (ros::ok())
 	{
 		if (lidar->IsFrameReady())
